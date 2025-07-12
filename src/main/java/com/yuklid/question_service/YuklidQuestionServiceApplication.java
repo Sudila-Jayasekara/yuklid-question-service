29db0c040540a9379ed54dff82111b05f212264a
Sudila-Jayasekara <mpsjayasekara@gmail.com>
2025-07-12 13:19:38 +0530

    project init

---
+++ src/main/java/com/yuklid/question_service/YuklidQuestionServiceApplication.java
@@ -0,0 +1,13 @@
+package com.yuklid.question_service;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class YuklidQuestionServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(YuklidQuestionServiceApplication.class, args);
+	}
+
+}
